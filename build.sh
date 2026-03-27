f1e801068b8d0bef5f7c030917efbe09f009537e
Max van Asten <maxvasten@gmail.com>
2026-03-27 21:20:31 +0100

    added build script

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+rm -rf ./dist/t5/*
+zip -r ./dist/t5/zm_randomizer.zip ./src/t5/zm_randomizer/
+
+rm -rf ./dist/t6/*
+zip -r ./dist/t6/zm_randomizer.zip ./src/t6/zm_randomizer/
+zip -r ./dist/t6/zm_gobblegums.zip ./src/t6/zm_gobblegums/
+zip -r ./dist/t6/zm_quests_demo.zip ./src/t6/zm_quests_demo/
+zip -r ./dist/t6/zm_tomb_challenge.zip ./src/t6/zm_tomb_challenge/
